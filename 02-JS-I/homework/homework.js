f1ee17459c4e75232f47d24fc37c3f62708643b0
Manuacvdo <manuelacevedo@live.com.ar>
2022-04-14 22:34:38 -0300

    marquitos

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -1,22 +1,24 @@
 // En estas primeras 6 preguntas, reemplaza `null` por la respuesta
 
 // Crea una variable "string", puede contener lo que quieras:
-const nuevaString = "holis"; 
+const string = "holanda"; 
+
+string = 'Manu';
 
 // Crea una variable numérica, puede ser cualquier número:
 const nuevoNum = "172";
 
 // Crea una variable booleana:
-const nuevoBool = null;
+const nuevoBool = true;
 
 // Resuelve el siguiente problema matemático:
 const nuevaResta = 10 - 5 === 5;
 
 // Resuelve el siguiente problema matemático:
-const nuevaMultiplicacion = 10 * null === 40 ;
+const nuevaMultiplicacion = 10 * 4 === 40 ;
 
 // Resuelve el siguiente problema matemático:
-const nuevoModulo = 21 % 5 === null;
+const nuevoModulo = 21 % 5 === 4.20
 
 
 // En los próximos 22 problemas, deberás completar la función.
@@ -31,7 +33,7 @@ function devolverString(str) {
   
 }
 
-function suma(x, y) {
+function suma(x, i) {
   // "x" e "y" son números
   // Suma "x" e "y" juntos y devuelve el valor
   // Tu código:
